cddcd4ef37300f39a44f325320a3253cb19b3ecd
Sunip88 <drzep4@gmail.com>
2018-09-09 15:29:22 +0200

    New file

---
+++ models/Users.py
@@ -0,0 +1,11 @@
+class User(object):
+    __id = None
+    username = None
+    __hashed_password = None
+    email = None
+
+    def __init__(self):
+        self.__id = -1
+        self.username = ""
+        self.email = ""
+        self.__hashed_password = ""
