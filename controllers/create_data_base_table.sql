1ab47404e1a5671e0b7919f65abc6918fa6b23b7
“sylwia30” <sylwusia30@gmail.com>
2018-09-09 16:36:36 +0200

    add new

--- controllers/create_data_base_table.sql
+++ controllers/create_data_base_table.sql
@@ -6,7 +6,19 @@ id serial,
 username VARCHAR (255),
 hashed_password VARCHAR(255),
 email VARCHAR (255) unique
+PRIMARY KEY (id)
 );
+
+CREATE TABLE message
+(
+id serial,
+from_id int,
+to_id int,
+message text,
+creation_date timestamp
+PRIMARY KEY (id)
+);
+
 
 
 
