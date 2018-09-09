dd693b092961e148dbb74e97d61fb00abe36fb47
Sunip88 <drzep4@gmail.com>
2018-09-09 15:40:59 +0200

    Added savetodb

--- models/Users.py
+++ models/Users.py
@@ -1,5 +1,6 @@
 from controllers import clcrypto
 
+
 class User(object):
     __id = None
     username = None
@@ -21,6 +22,16 @@ class User(object):
     def hashed_password(self):
         return self.__hashed_password
 
-    #password_hash missing
     def set_password(self, password, salt):
         self.__hashed_password = clcrypto.password_hash(password, salt)
+
+    def save_to_db(self, cursor):
+        if self.__id == -1:
+            # saving new instance using prepared statements
+            sql = """INSERT INTO Users(username, email, hashed_password)
+                     VALUES(%s, %s, %s) RETURNING id"""
+            values = (self.username, self.email, self.hashed_password)
+            cursor.execute(sql, values)
+            self.__id = cursor.fetchone()[0]  # albo cursor.fetchone()['id']
+            return True
+      return False
