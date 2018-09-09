5cc79bb021af76fc1ac33cbaf32e004cbc1a30b2
Sunip88 <drzep4@gmail.com>
2018-09-09 15:20:19 +0200

    Corrected typos

--- sql_connect.py
+++ sql_connect.py
@@ -1,11 +1,12 @@
 from psycopg2 import connect, OperationalError
 
+
 def create_connection(db_name):
     username = "postgres"
-    passwd = "coderslab"
+    password = "coderslab"
     hostname = "localhost"  # lub "127.0.0.1"
     try:
-        cnx = connect(user=username, password=passwd, host=hostname, database=db_name)
+        cnx = connect(user=username, password=password, host=hostname, database=db_name)
         cnx.autocommit = True
         print("Połączenie udane")
     except OperationalError as error:
@@ -13,22 +14,22 @@ def create_connection(db_name):
         print("Brak połączenia")
     return cnx
 
+
 def execute_sql(cnx, sql_file):
     cursor = cnx.cursor()
     file = open(sql_file, 'r')
     sql_text = file.read()
     file.close()
-
-    sqlCommand = sql_text.split(";")
-    print(sqlCommand)
-
-    for command in sqlCommand:
+    sqlcommand = sql_text.split(";")
+    print(sqlcommand)
+    for command in sqlcommand:
         try:
             cursor.execute(command)
             print("Baza utworzona")
         except Exception as error:
             print("Nie udało się utorzyć tabeli, błąd", error)
     cursor.close()
+
 
 if __name__ == "__main__":
     connection = create_connection("postgres")
